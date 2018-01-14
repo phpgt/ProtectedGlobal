fccb558b96e35bf4766104c570a438515d1295c9
Greg Bowler <greg.bowler@g105b.com>
2018-01-14 17:46:15 +0000

    Add failing tests for whitelist

--- test/unit/GlobalOverrideTest.php
+++ test/unit/GlobalOverrideTest.php
@@ -7,26 +7,78 @@ use PHPUnit\Framework\TestCase;
 
 class GlobalOverrideTest extends TestCase {
 	public function testDeregister() {
-		$myArray = [
+		$testGlobals = [
 			"global1" => [
 				"somekey" => "somevalue",
 			]
 		];
-		self::assertArrayHasKey("somekey", $myArray["global1"]);
-		GlobalOverride::deregister($myArray);
-		self::assertArrayNotHasKey("somekey", $myArray["global1"]);
-		self::assertNotNull($myArray);
+		self::assertArrayHasKey("somekey", $testGlobals["global1"]);
+		GlobalOverride::deregister($testGlobals);
+		self::assertArrayNotHasKey("somekey", $testGlobals["global1"]);
+		self::assertNotNull($testGlobals);
 	}
 
 	public function testOverride() {
-		$myArray = [
+		$testGlobals = [
 			"global1" => [
 				"somekey" => "somevalue",
 			]
 		];
-		self::assertEquals("somevalue", $myArray["global1"]["somekey"]);
-		GlobalOverride::override($myArray);
+		self::assertEquals("somevalue", $testGlobals["global1"]["somekey"]);
+		GlobalOverride::override($testGlobals);
 		self::expectException(ProtectedGlobalException::class);
-		echo $myArray["global1"]["somekey"];
+		echo $testGlobals["global1"]["somekey"];
+	}
+
+	public function testDeregisterWithWhiteList() {
+		$time = time();
+		$testGlobals = [
+			"global1" => [
+				"time" => $time,
+				"version" => PHP_VERSION,
+			],
+			"global2" => [
+				"directory_separator" => DIRECTORY_SEPARATOR,
+				"test_dir" => __DIR__,
+			]
+		];
+
+		self::assertEquals($time, $testGlobals["global1"]["time"]);
+
+		GlobalOverride::override($testGlobals, [
+			"global1" => ["version"],
+			"global2" => ["directory_separator"],
+		]);
+
+		self::assertEquals(
+			PHP_VERSION,
+			$testGlobals["global1"]["version"]
+		);
+		self::assertEquals(
+			DIRECTORY_SEPARATOR,
+			$testGlobals["global1"]["directory_separator"]
+		);
+	}
+
+	public function testDeregisterWithWhiteListThrows() {
+		$time = time();
+		$testGlobals = [
+			"global1" => [
+				"time" => $time,
+				"version" => PHP_VERSION,
+			],
+			"global2" => [
+				"directory_separator" => DIRECTORY_SEPARATOR,
+				"test_dir" => __DIR__,
+			]
+		];
+
+		GlobalOverride::override($testGlobals, [
+			"global1" => ["version"],
+			"global2" => ["directory_separator"],
+		]);
+
+		self::expectException(ProtectedGlobalException::class);
+		echo $testGlobals["global1"]["time"];
 	}
 }
