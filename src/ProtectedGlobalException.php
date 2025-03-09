b8ac7187e1e3047877a68b21416fa545812ed96a
Greg Bowler <greg.bowler@g105b.com>
2025-03-09 13:53:23 +0000

    ci: php 8.4 update (#176)

--- src/ProtectedGlobalException.php
+++ src/ProtectedGlobalException.php
@@ -5,12 +5,4 @@ namespace Gt\ProtectedGlobal;
 use Exception;
 use Throwable;
 
-class ProtectedGlobalException extends Exception {
-	public function __construct(
-		string $message = "",
-		int $code = 0,
-		Throwable $previous = null
-	) {
-		parent::__construct($message, $code, $previous);
-	}
-}
+class ProtectedGlobalException extends Exception {}
